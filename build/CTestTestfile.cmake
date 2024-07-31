7af51d4fd24fedadfcc338e56d733dce8ee2bce7
Federico <federico.mastroforti@gmail.com>
2024-07-31 23:24:06 +0200

    adding build directory to .gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/federico/federico/rat4ccc
-# Build directory: /home/federico/federico/rat4ccc/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
